b38cc8e66d9311262b7b675005bb8d85f2bab4a5
theismunk <s164022@student.dtu.dk>
2020-10-30 13:44:14 +0100

    fixed delta value

--- Part_5_Robot_Control/Problem7_9.m
+++ Part_5_Robot_Control/Problem7_9.m
@@ -48,13 +48,9 @@ I4 = D4(2,2);
 
 n = 53;
 J_M = 1320*10^(-7);
-Delta2 = 1;
+Delta2 = 0.34;
 
-% q2 = linspace(pi/6,3*pi);
-% q3 = linspace(1.35, 3);
-% q4 = linspace(-pi/2, 5*pi/4);
-
-
+% helpers
 K0 = m2*(1/2*L2-Delta2)^2 + m3*(1/2*L3)^2;
 K1 = 1/2*(I4 + 1/4*m4*a4^2);
 K2 = 1/2*(I2 - I2zz + I3 - I3yy + K0);
@@ -72,18 +68,17 @@ D22 = @(q2,q3,q4) K3 + 2*f1(q3) + f2(q3)*sin(q4);
 D33 = K4;
 D44 = 2*K1;
 
+% lower bound
 x0 = [pi/6; 1.35; -pi/2];
+
+% upper bound
 x1 = [3*pi; 3; 5*pi/4];
-A = eye(3);
 
-
+% find maximum value
 sol1 = fmincon(@(x) -D11(x(1),x(2),x(3)), x0, [], [], [], [], x0, x1);
-sol1;
-
 sol2 = fmincon(@(x) -D22(x(1),x(2),x(3)), x0, [], [], [], [], x0, x1);
-sol2;
 
-
+% construct supD vector
 supD = [D11(sol1(1), sol1(2), sol1(3)); D22(sol2(1), sol2(2), sol2(3)); D33; D44];
 
 % my name is
